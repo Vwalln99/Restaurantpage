adae3b54f5e6abb4bde3c684968dd3f91a592d0a
Vwalln99 <wallner.viktoria@gmx.net>
2023-08-29 08:10:26 +0200

    init commit

--- src/menu.js
+++ src/menu.js
@@ -12,12 +12,14 @@ export const menuModule = (section) => {
   menuText.className = "text_menu";
   menuText.textContent = "Check out our delicious vegan menu options!";
 
-  const images = addImages(['./img/breakfast_platter.jpg', './img/main_platter.jpg', './img/pasta farfalle.jpg', './img/cake.jpg', './img/cupcakes.jpg', './img/macarons.jpg']);
+  const images = addImages(['./img/breakfast_platter.jpg', './img/main_platter.jpg', './img/pasta farfalle.jpg', './img/cake.jpg', './img/cupcakes.jpg', './img/macarons.jpg', './img/smoothie.jpg', './img/avocado.jpg']);
   images[0].classList.add('img3');
   images[1].classList.add('img4');
   images[2].classList.add('img5');
   images[3].classList.add('img6');
   images[4].classList.add('img7');
   images[5].classList.add('img8');
+  images[6].classList.add('img10');
+  images[7].classList.add('img11');
   appendImages(section, [menuHeading, menuText, ...images]);
 };
